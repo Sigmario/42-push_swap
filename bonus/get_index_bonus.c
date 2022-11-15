3ed4f366cd8428672eba01d142290bf9661450fe
Jules MUNTZ-BERGER <julmuntz@e1r4p7.clusters.42paris.fr>
2022-11-15 13:42:08 +0100

    Removing idle functions

--- bonus/get_index_bonus.c
+++ bonus/get_index_bonus.c
@@ -6,7 +6,7 @@
 /*   By: julmuntz <julmuntz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 11:25:57 by julmuntz          #+#    #+#             */
-/*   Updated: 2022/11/10 18:28:43 by julmuntz         ###   ########.fr       */
+/*   Updated: 2022/11/15 13:39:39 by julmuntz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,52 +39,6 @@ void	get_index(t_stack **ptr, int size)
 			end->index = size;
 		size--;
 	}
-}
-
-int	get_1stmax(t_stack **ptr)
-{
-	t_stack	*current;
-	int		max_value_pos;
-
-	get_pos(ptr);
-	max_value_pos = INT_MIN;
-	current = *ptr;
-	while (current)
-	{
-		if (current->index > max_value_pos)
-			max_value_pos = current->index;
-		current = current->next;
-	}
-	return (max_value_pos);
-}
-
-int	get_2ndmax(t_stack **ptr)
-{
-	int		size;
-	t_stack	*current;
-	int		max_value_pos;
-	int		second_max_value_pos;
-
-	get_pos(ptr);
-	size = stacksize(*ptr);
-	if (size < 2)
-		return (0);
-	max_value_pos = INT_MIN;
-	second_max_value_pos = INT_MIN;
-	current = *ptr;
-	while (current)
-	{
-		if (current->index >= max_value_pos)
-		{
-			second_max_value_pos = max_value_pos;
-			max_value_pos = current->index;
-		}
-		else if (current->index >= second_max_value_pos
-			&& current->index != max_value_pos)
-			second_max_value_pos = current->index;
-		current = current->next;
-	}
-	return (second_max_value_pos);
 }
 
 void	get_pos(t_stack **ptr)
